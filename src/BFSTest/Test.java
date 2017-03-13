e24264b66ef14e0387249b119967049b726f5bae
123456 <825520501@qq.com>
2017-03-14 01:36:40 +0800

    第二次提交

--- src/BFSTest/Test.java
+++ src/BFSTest/Test.java
@@ -7,9 +7,6 @@ public class Test {
     public static void main(String[] args) {
         MyGraph myGraph = new MyGraph();
         myGraph.initGraph();
-//        for (int i = 0; i < myGraph.getGraphNodes().size(); i++) {
-//            new BFSearch().searchTraversing(myGraph.getGraphNodes().get(i));
-//        }
         new BFSearch().searchTraversing(myGraph.getGraphNodes().get(0));
     }
 
